d2991f3a1413c10af2f8b6151527548302a873c6
Gowtham <gowthambabu8@gmail.com>
2026-02-04 10:52:05 +0530

    code changes for roboshop

---
+++ roboshop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SG_ID="sg-038655dad535c0654"
+AMI_ID="ami-0220d79f3f480ecf5"
+INSTANCE_TYPE="t3.micro"
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count 1 \
+    --instance-type $INSTANCE_TYPE \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]")
+
+    if [ $instance -eq "frontend" ]; then 
+        IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PublicIpAddress" \
+        --output text)
+    else
+        IP=$(aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query "Reservations[*].Instances[*].PrivateIpAddress" \
+        --output text)
+    fi
+
+    echo "IP is $IP"
+done
