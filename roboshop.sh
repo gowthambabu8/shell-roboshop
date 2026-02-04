25623447a4055ae57fdb3875051cba39b57506cd
Gowtham <gowthambabu8@gmail.com>
2026-02-04 10:53:19 +0530

    code changes for roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -12,7 +12,7 @@ do
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]")
 
-    if [ $instance -eq "frontend" ]; then 
+    if [ $instance == "frontend" ]; then 
         IP=$(aws ec2 describe-instances \
         --instance-ids $INSTANCE_ID \
         --query "Reservations[*].Instances[*].PublicIpAddress" \
