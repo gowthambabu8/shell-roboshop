53656c5b28c1700fd4313967bd961ba1824be6c7
Gowtham <gowthambabu8@gmail.com>
2026-02-09 07:48:40 +0530

    code changed

--- mongodb.sh
+++ mongodb.sh
@@ -10,12 +10,12 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-robo"
 LOGS_FILE="/var/log/shell-robo/$0.log"
 
+mkdir -p $LOGS_FOLDER
+
 if [ $USERID -ne 0 ]; then 
     echo "Please use super user to install" | tee -a $LOGS_FILE
     exit 1
 fi
-
-mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
