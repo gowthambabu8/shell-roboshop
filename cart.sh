cfaa0e5a75ed642a8233afc774616cb723c18d0e
Gowtham <gowthambabu8@gmail.com>
2026-02-09 11:22:51 +0530

    code changed

---
+++ cart.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e #ERR
+
+trap 'echo "There is error in line $LINENO, Command: $BASH_COMMAND"' ERR
+
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-robo"
+LOGS_FILE="/var/log/shell-robo/$0.log"
+SCRIPT_DIR=$PWD
+MONGODB_HOST="mongodb.happielearning.com"
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then 
+    echo "Please use super user to install" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2.... FAILED" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2.... SUCCESS" | tee -a $LOGS_FILE
+fi    
+}
+
+dnf module list nodejs &>>$LOGS_FILE
+VALIDATE $? "listing nodejs modules...."
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "disabling latest nodejs module...."
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "enabling version 20 for nodejs...."
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "installing nodejs required version...."
+
+USER_CHECK="roboshop"
+if id "$USER_CHECK" &>/dev/null; then 
+    echo -e "user roboshop already exists" | tee -a $LOGS_FILE
+else
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+fi
+
+mkdir -p /app &>>$LOGS_FILE
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOGS_FILE
+VALIDATE $? "downloading cart source code...."
+cd /app &>>$LOGS_FILE
+
+rm -rf /app/* 
+unzip /tmp/cart.zip
+VALIDATE $? "unzipping cart source code...."
+npm install
+VALIDATE $? "installing npm build tool...."
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+
+systemctl daemon-reload &>>$LOGS_FILE
+VALIDATE $? "thread daemon reload...."
+systemctl enable cart &>>$LOGS_FILE
+VALIDATE $? "enabling cart service...."
+systemctl start cart &>>$LOGS_FILE
+VALIDATE $? "starting cart service..."
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y &>>$LOGS_FILE
+VALIDATE $? "installing mongodb client...."
+mongosh --host $MONGODB_HOST </app/db/master-data.js
+VALIDATE $? "data loading completed...."
---
+++ redis.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e #ERR
+
+trap 'echo "There is error in line $LINENO, Command: $BASH_COMMAND"' ERR
+
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-robo"
+LOGS_FILE="/var/log/shell-robo/$0.log"
+SCRIPT_DIR=$PWD
+MONGODB_HOST="mongodb.happielearning.com"
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then 
+    echo "Please use super user to install" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2.... FAILED" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2.... SUCCESS" | tee -a $LOGS_FILE
+fi    
+}
+
+dnf module disable redis -y &>>$LOGS_FILE
+VALIDATE $? "disabling latest module redis..."
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "enabling module 7 for redis...."
+dnf install redis -y &>>$LOGS_FILE
+VALIDATE $? "installing redis...."
+sed -i -e "s/127.0.0.1/0.0.0.0/g" -e "/protected mode/c protected mode no" /etc/redis/redis.conf
+systemctl enable redis &>> LOGS_FILE
+VALIDATE $? "enabling redis service..."
+systemctl start redis &>> LOGS_FILE
+VALIDATE "starting redis service...."
