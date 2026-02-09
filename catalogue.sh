f856c2c9bd81d293b3a4d7ff3f8de79b309ce749
Gowtham <gowthambabu8@gmail.com>
2026-02-09 08:06:10 +0530

    code changed

---
+++ catalogue.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e #ERR
+
+trap 'echo "There is error in line $LINENO, Command: $BASH_COMMAND"' ERR
+
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-robo"
+LOGS_FILE="/var/log/shell-robo/$0.log"
+
+mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then 
+    echo "Please use super user to install" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2.... FAILED" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2.... SUCCESS" | tee -a $LOGS_FILE
+fi    
+}
+
+dnf module list nodejs
+dnf module disbable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+
+mkdir /app
